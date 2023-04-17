6d60f2593b7986a55b133bcc26115ed489242e01
ptan01 <ptanjim01@gmail.com>
2023-04-17 23:57:19 +0600

    use useEffect and set user in nav bar

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,5 +1,5 @@
-import React, { createContext, useState } from 'react';
-import {createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword} from 'firebase/auth'
+import React, { createContext, useEffect, useState } from 'react';
+import {createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut} from 'firebase/auth'
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext(null)
@@ -17,11 +17,24 @@ const AuthProvider = ({children}) => {
     const loginUser = (email, password) =>{
         return signInWithEmailAndPassword(auth, email, password)
     }
+
+    const logOut = () =>{
+       return signOut(auth)
+    }
     
+    useEffect(()=>{
+        onAuthStateChanged(auth, (currentUser)=>{
+            setUser(currentUser)
+        })
+    },[])
+
+
+
     const authInfo = {
         user,
         createUser,
         loginUser,
+        logOut,
     }
 
 
--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Header.css'
 import logo from "../../assets/images/Logo.svg"
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../Provider/AuthProvider';
 const Header = () => {
+    const { user ,logOut} = useContext(AuthContext)
+    const handleSignOut =()=>{
+        logOut()
+        .then(resut =>{
+            console.log(resut)
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    }
     return (
         <div className='header'>
             <img src={logo} alt="" />
-            <div>
-                <Link to="/">Shop</Link>
+         
+               
+               <div>
+               <Link to="/">Shop</Link>
                 <Link to="/order">Order</Link>
                 <Link to="/inventory">Inventory</Link>
                 <Link to="/login">LogIn</Link>
                 <Link to="/signup">Sign Up</Link>
-            </div>
+                {
+                    user && <span className='text-white'>{user.email} <button onClick={handleSignOut}>Sign Out</button> </span>
+                }
+               </div>
+                
+        
+           
         </div>
     );
 };
